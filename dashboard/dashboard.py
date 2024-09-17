ac53a4e5c030b5ec510c3ed35d97419c447b5b14
fthanb <famukh16@gmail.com>
2024-09-18 06:56:02 +0700

    Update notebook

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -67,10 +67,11 @@ for i, polusi in enumerate(melt['Polusi'].unique()):
         ax.legend(title='', fontsize=10) 
         st.pyplot(fig)
 
-with st.expander("Konklusi: "):
+with st.expander("Kesimpulan : "):
     st.write(
-        "Jumlah partikel-partikel yang terdapat dalam udara di sekitar Guanyuan termasuk sedikit (kategori: relatif aman), "
-        "kecuali jumlah partikel PM2.5 yang termasuk ke dalam kategori: tidak aman. Berdasarkan Indeks Standar Pencemar Udara."
+        "Rata-rata jumlah partikel polusi udara di sekitar Guanyuan termasuk relatif aman (kategori: Baik & Sedang),"
+        "kecuali jumlah partikel PM2.5 yang termasuk tidak aman (kategori: Tidak Sehat)" 
+        "Berdasarkan Indeks Standar Pencemar Udara."
     )
     st.image("ispu.png", caption="Indeks Standar Pencemar Udara (ISPU)", use_column_width=True)
     st.image("ispu2.png", caption="https://ditppu.menlhk.go.id/portal/read/indeks-standar-pencemar-udara-ispu-sebagai-informasi-mutu-udara-ambien-di-indonesia", use_column_width=True)
@@ -109,13 +110,13 @@ st.subheader(" ")
 st.subheader("Arah Angin & Kecepatan Angin (m/s)")
 st.dataframe(arah_angin, width=1000) 
 
-with st.expander("Konklusi: "):
+with st.expander("Kesimpulan : "):
     st.write(
         """
-        - Pada musim dingin, rata-rata suhu mencapai (-0.17°C), arah mata angin sering ke NorthEast (1,8 m/s).
-        - Pada musim gugur, rata-rata suhu mencapai (-13.0°C), arah mata angin sering ke NorthEast (1,4 m/s).
-        - Pada musim panas, rata-rata suhu mencapai (-26.1°C), arah mata angin sering ke NorthEast (1,4 m/s).
-        - Pada musim semi, rata-rata suhu mencapai (-15.0°C), arah mata angin sering ke Southwest (2,0 m/s).
+        - Pada musim dingin, rata-rata suhu mencapai (-0.17°C), mata angin sering ke arah NorthEast (1,8 m/s).
+        - Pada musim gugur, rata-rata suhu mencapai (-13.0°C), mata angin sering ke arah NorthEast (1,4 m/s).
+        - Pada musim panas, rata-rata suhu mencapai (-26.1°C), mata angin sering ke arah NorthEast (1,4 m/s).
+        - Pada musim semi, rata-rata suhu mencapai (-15.0°C), mata angin sering ke arah Southwest (2,0 m/s).
         - Curah hujan ringan di semua musim.
         """
     )
